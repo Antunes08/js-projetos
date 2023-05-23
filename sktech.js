de777f175e4b9281920d7195d6d4158ae0e04768
Antunes08 <134344089+Antunes08@users.noreply.github.com>
2023-05-22 22:37:29 -0300

    sktech.js

--- sktech.js
+++ sktech.js
@@ -1,31 +1,16 @@
-let cor;
-let circuloX;// horizontal
-let circuloY;// vertical
-
-
 function setup() {
-  random(0,255)
-  createCanvas(400, 400);//width x height
-  background("purple");
-  cor = color(random(0,255), random(0,255), random(0,255));
-  circuloX = [0,0,0];
-  circuloY = [random(height),random(height),random(height)];
+  createCanvas(600, 600);
+   background("black"); 
 }
-//circuloX = 0,0,0
-//circuloY = 300,150,50
 
-  function draw() { 
-  fill(cor);
-    
-    console.log(circuloX.length);
-    for(let contador in circuloX){
-      console.log(contador);
-       circle(circuloX[contador], circuloY[contador], 50);
-      circuloX[contador]+= random(0,3);  
-      circuloY[contador]+= random(-3, 3);
-      
-      if(circuloX[contador]>= width){
-        circuloX[contador]= 0;
-        circuloY[contador]= random(height);
-      }
-    }
+function draw() {
+ 
+  stroke ("purple")
+  fill("red")
+  //console.log(mouseIsPressed);
+  if(mouseIsPressed) {
+   rect (mouseX, mouseY, 20, 35);   
+  }
+  
+  
+}
